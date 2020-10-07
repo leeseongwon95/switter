5dba42e823c23db8634cead92d5889693b6e0706
leeseongwon95 <67531188+leeseongwon95@users.noreply.github.com>
2020-10-07 16:39:36 +0900

    Log Out

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,5 +1,19 @@
+import { authService } from "fbase";
 import React from "react";
+import { useHistory } from "react-router-dom";
 
-const Profile = () => <span>Profile</span>;
+const Profile = () => {
+  const history = useHistory();
+  const onLogOutClick = () => {
+    authService.signOut();
+    history.push("/");
+    // react hooks 로 profile.js 에서 직접 할수도있다.
+  };
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
+};
 
 export default Profile;
