6d4d142848d8aca5dc57645feedde01dba50e127
leeseongwon95 <67531188+leeseongwon95@users.noreply.github.com>
2020-10-07 16:52:34 +0900

    Form and Databse Setup

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,31 @@
 import React from "react";
+import { useState } from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [sweet, setSweet] = useState("");
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setSweet(value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={sweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Sweet" />
+      </form>
+    </div>
+  );
+};
 
 export default Home;
