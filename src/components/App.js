4c50b6210c97a81d828307955bcd3b59aac9e0ac
leeseongwon95 <67531188+leeseongwon95@users.noreply.github.com>
2020-10-09 16:28:34 +0900

    Update Profile Bugfix

--- src/components/App.js
+++ src/components/App.js
@@ -11,15 +11,32 @@ function App() {
       // onAuthStateChanged state 변화를 다루고 있음
       if (user) {
         // 로그인 로그아웃 할 때 일어나고 어플리케이션이 초기화 될 때 일어남
-        setUserObj(user);
+        setUserObj({
+          // 왜 setUserObj(user) 가 아닌가? 오브젝트의 크기가 너무 커져서 리액트가 어떤 걸 랜더링 할 지 모른다
+          displayName: user.displayName,
+          uid: user.uid,
+          updateProfile: (args) => user.updateProfile(args), // 이 function은 원하는 function 을 얻기 위한 중간 function이다
+        });
       }
       setInit(true);
     });
   }, []);
+  const refreshUser = () => {
+    const user = authService.currentUser;
+    setUserObj({
+      displayName: user.displayName,
+      uid: user.uid,
+      updateProfile: (args) => user.updateProfile(args), // 이 function은 원하는 function 을 얻기 위한 중간 function이다
+    });
+  };
   return (
     <>
       {init ? (
-        <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} />
+        <AppRouter
+          refreshUser={refreshUser}
+          isLoggedIn={Boolean(userObj)}
+          userObj={userObj}
+        />
       ) : (
         "Initializing..."
       )}
